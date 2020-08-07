f1b80583fbd1292db1d4517473a8bf7ea3316663
Shreyas <shreyaskhachane3@gmail.com>
2020-08-07 13:27:08 +0530

    version 1.01

--- blogs/urls.py
+++ blogs/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/',include('users.urls')),
     path('register/', user_views.register,name='register'),
     path('profile/',user_views.profile,name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
